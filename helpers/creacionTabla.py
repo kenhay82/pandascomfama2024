a0853bd4de1cb157524bd7c52fb03aa8c678408a
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-24 20:32:57 -0500

    nt3 se crea ruina generando tabla

--- helpers/creacionTabla.py
+++ helpers/creacionTabla.py
@@ -1,39 +1,35 @@
-import pandas as pd 
+import pandas as pd
 from bs4 import BeautifulSoup
 
-def crearTabla(dataFrame,nombreHTML):
-
-    #obtener una tabla de html desde un DataFrame de python
+def crearTabla(dataFrame,nombreTabla):
+    
+    #crear un HTML desde un Datframe de python
     archivoHTML=dataFrame.to_html()
-
-    #Definir la ruta donde se va a guardar esa tabla
-    rutaArchivo=f"tables/{nombreHTML}.html"
-
-    #Construir un encabezado <head> del archivo generado
-    encabezadoHTML='''
+    
+    #Establecemos la ruta donde vamos a guardar la tabla
+    rutaArchivo=f"tables/{nombreTabla}.html"
+    
+    #Generamos una estructura HTML
+    encabezadoHTML=''' 
         <!DOCTYPE html>
-        <html lang="en">
+        <html>
             <head>
                 <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Document</title>
+                <title>tablas</title>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
             </head>
-            <body>
+        </html>
     '''
-
-    #Utilizo BS4 para optimizar mi tabla
+    
+    #Adaptar los estilos de la tabla
     sopa=BeautifulSoup(archivoHTML,'html.parser')
-
-    #Encontrar todas las FILAS 'tr' eliminar atributo style
+    
     for tr in sopa.find_all('tr'):
         tr.attrs.pop('style',None)
-
+    
     archivoHTML=str(sopa)
-
-    #Agregar la clase table y table striped de bootstrap
-    archivoHTML=archivoHTML.replace('<table border="1" class="dataFrame">','<table class="table table-striped">')
-
-    #Escribir la tabla y generarla en el proyecto
+    
+    archivoHTML=archivoHTML.replace('<table border="1" class="dataframe">','<table class="table table-striped">')
+    
     with open(rutaArchivo,"w") as archivo:
         archivo.write(f"{encabezadoHTML}\n{archivoHTML}\n</body>\n</html>")
