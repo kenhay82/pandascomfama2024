c3295bdf0a4f79f02d2540eb26c32bd6fad76b2c
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-01-24 20:18:13 -0500

    nt3 se crea ruina generando tabla

--- analysis/analisisDeCanasta.py
+++ analysis/analisisDeCanasta.py
@@ -1,12 +1,14 @@
 #1. Importar el paquete o paquetes con los que voy
 #a analizar la informacion
 import pandas as pd
+from helpers.creacionTabla import crearTabla
 
 def analizarCanastaBasica():
     #2. Sin importar la fuente (sql, xls, JSON, csv...)
     #crear una tabla tabulada que se lama DATAFRAME
     tabla=pd.read_csv('./data/bdcanasta.csv')
     #print(tabla)
+    crearTabla(tabla,"canastabasica")
 
     #3. Aplico filtros para limpiar o seleccionar datos
 
